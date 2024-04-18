639268faaf9eefc709a2840e7fb9a165d6cb01ad
Rohan Reddy <rohanreddy888@gmail.com>
2024-04-19 02:12:22 +0530

    Initial Commit

---
+++ src/app/components/AuthProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+export default function AuthProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Auth0Provider
+      domain="dev-emy5488k5kw8za0s.us.auth0.com"
+      clientId="gEM9GBpZ8U6IKFycRfvEQkRPDSgOOXp0"
+      authorizationParams={{
+        redirect_uri: "http://localhost:3000",
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  );
+}
