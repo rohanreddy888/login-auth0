db9f83ee7a3a4968a9f1750f9cb0edeed3f3c494
Rohan Reddy <rohanreddy888@gmail.com>
2024-04-19 02:16:21 +0530

    UI Tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,31 @@ export default function Home() {
   const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
 
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center gap-4">
+    <section className="min-h-screen flex flex-col justify-center items-center gap-6">
       {isAuthenticated ? (
         <>
-          <h1 className="text-2xl">
+          <h1 className="text-4xl">
             Hello, <b>{user?.name}</b>
           </h1>
           <button
-            className="bg-sky-600 px-6 py-2 rounded shadow"
+            className="bg-red-200 text-red-600 px-6 py-2 rounded shadow font-bold"
             onClick={() => logout()}
           >
             Logout
           </button>
         </>
       ) : (
-        <button
-          className="bg-sky-600 px-6 py-2 rounded shadow"
-          onClick={() => loginWithRedirect()}
-        >
-          Login
-        </button>
+        <>
+          <h1 className="text-4xl">
+            <b>Auth0 Login Demo</b>
+          </h1>
+          <button
+            className="bg-sky-200 text-sky-600 px-6 py-2 rounded shadow font-bold"
+            onClick={() => loginWithRedirect()}
+          >
+            Login
+          </button>
+        </>
       )}
     </section>
   );
